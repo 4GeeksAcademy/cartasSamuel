07bb68605a8ba1f19cc2fa09d61a32cec4d9f1b7
Jsamuel1919 <156345975+Jsamuel1919@users.noreply.github.com>
2024-02-23 23:22:34 +0000

    first

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let palos = ["♦", "♥", "♠", "♣️ "];
+let cartas = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let i = document.getElementById("iconoArriba");
+let j = document.getElementById("numeroCarta");
+let x = document.getElementById("iconoAbajo");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function visualizarCartas(palos, cartas) {
+  let baraja = palos[Math.floor(Math.random() * palos.length)];
+  let numeros = cartas[Math.floor(Math.random() * cartas.length)];
+  i.textContent = baraja;
+  j.textContent = numeros;
+  x.textContent = baraja;
+}
+visualizarCartas(palos, cartas);
